2b1f105a00d39964560994749ff71b4dd1ed1652
AntonOshurek <oshurekweb@gmail.com>
2022-05-28 16:12:51 +0200

    maping films in films list

---
+++ project/src/data/films-data.ts
@@ -0,0 +1,132 @@
+type Film = {
+  img: string,
+  title: string,
+  link: string,
+  key: number,
+}
+
+const films: Film[] = [
+  {
+    img: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    title: 'Fantastic Beasts: The Crimes of Grindelwald',
+    link: 'film-page.html',
+    key: 101,
+  },
+  {
+    img: 'img/bohemian-rhapsody.jpg',
+    title: 'Bohemian Rhapsody',
+    link: 'film-page.html',
+    key: 102,
+  },
+  {
+    img: 'img/macbeth.jpg',
+    title: 'Macbeth',
+    link: 'film-page.html',
+    key: 103,
+  },
+  {
+    img: 'img/aviator.jpg',
+    title: 'Aviator',
+    link: 'film-page.html',
+    key: 104,
+  },
+  {
+    img: 'img/we-need-to-talk-about-kevin.jpg',
+    title: 'We need to talk about Kevin',
+    link: 'film-page.html',
+    key: 105,
+  },
+  {
+    img: 'img/what-we-do-in-the-shadows.jpg',
+    title: 'What We Do in the Shadows',
+    link: 'film-page.html',
+    key: 106,
+  },
+  {
+    img: 'img/revenant.jpg',
+    title: 'Revenant',
+    link: 'film-page.html',
+    key: 107,
+  },
+  {
+    img: 'img/johnny-english.jpg',
+    title: 'Johnny English',
+    link: 'film-page.html',
+    key: 108,
+  },
+  {
+    img: 'img/shutter-island.jpg',
+    title: 'Shutter Island',
+    link: 'film-page.html',
+    key: 109,
+  },
+  {
+    img: 'img/pulp-fiction.jpg',
+    title: 'Pulp Fiction',
+    link: 'film-page.html',
+    key: 110,
+  },
+  {
+    img: 'img/no-country-for-old-men.jpg',
+    title: 'No Country for Old Men',
+    link: 'film-page.html',
+    key: 111,
+  },
+  {
+    img: 'img/snatch.jpg',
+    title: 'Snatch',
+    link: 'film-page.html',
+    key: 112,
+  },
+  {
+    img: 'img/moonrise-kingdom.jpg',
+    title: 'Moonrise Kingdom',
+    link: 'film-page.html',
+    key: 113,
+  },
+  {
+    img: 'img/seven-years-in-tibet.jpg',
+    title: 'Seven Years in Tibet',
+    link: 'film-page.html',
+    key: 114,
+  },
+  {
+    img: 'img/midnight-special.jpg',
+    title: 'Midnight Special',
+    link: 'film-page.html',
+    key: 115,
+  },
+  {
+    img: 'img/war-of-the-worlds.jpg',
+    title: 'War of the Worlds',
+    link: 'film-page.html',
+    key: 116,
+  },
+  {
+    img: 'img/dardjeeling-limited.jpg',
+    title: 'Dardjeeling Limited',
+    link: 'film-page.html',
+    key: 117,
+  },
+  {
+    img: 'img/orlando.jpg',
+    title: 'Orlando',
+    link: 'film-page.html',
+    key: 118,
+  },
+  {
+    img: 'img/mindhunter.jpg',
+    title: 'Mindhunter',
+    link: 'film-page.html',
+    key: 119,
+  },
+  {
+    img: 'img/midnight-special.jpg',
+    title: 'Midnight Special',
+    link: 'film-page.html',
+    key: 120,
+  },
+];
+
+export { films };  export type { Film };
+

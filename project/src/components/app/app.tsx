45ef143a2b828cf17443a82c50488b042d79f45e
AntonOshurek <oshurekweb@gmail.com>
2022-05-28 13:21:01 +0200

    add page content block

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,12 @@
 import FilmCard from '../film-card/film-card';
+import PageContent from '../page-content/page-content';
 
 function App(): JSX.Element {
   return (
-    <FilmCard/>
+    <>
+      <FilmCard/>
+      <PageContent/>
+    </>
   );
 }
 

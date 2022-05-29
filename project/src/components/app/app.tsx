058419785ce6a8f400ccd1dbf7aaab70525ea27f
AntonOshurek <oshurekweb@gmail.com>
2022-05-29 16:39:42 +0200

    add login page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import MainPage from '../../pages/main-page';
 import MyList from '../../pages/my-list';
+import Login from '../../pages/login';
 
 
 type FilmCardObject = {
@@ -23,6 +24,7 @@ function App({filmCard}: AppProps): JSX.Element {
       <Routes>
         <Route path='/' element={<MainPage filmCard={filmCard}/> } />
         <Route path='/mylist' element={<MyList/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );

85a23a87109a7d805bb8c4c05547b00f198d1d47
AntonOshurek <oshurekweb@gmail.com>
2022-05-29 16:52:57 +0200

    remove film card info component

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,4 +1,3 @@
-import FilmCardInfo from '../film-card-info/film-card-info';
 import Logo from '../logo/logo';
 import UserBlock from '../user-block/user-block';
 
@@ -30,7 +29,34 @@ function FilmCard({filmCard}: FilmCardProps): JSX.Element {
       </header>
 
       <div className="film-card__wrap">
-        <FilmCardInfo filmCard={filmCard}/>
+        <div className="film-card__info">
+          <div className="film-card__poster">
+            <img src={filmCard.poster} alt={filmCard.title} width="218" height="327" />
+          </div>
+
+          <div className="film-card__desc">
+            <h2 className="film-card__title">{filmCard.title}</h2>
+            <p className="film-card__meta">
+              <span className="film-card__genre">{filmCard.genre}</span>
+              <span className="film-card__year">20{filmCard.year}14</span>
+            </p>
+
+            <div className="film-card__buttons">
+              <button className="btn btn--play film-card__button" type="button">
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list film-card__button" type="button">
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );

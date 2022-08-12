7c59b045e0965e310bfcb36743ee4ec376c05c57
AntonOshurek <oshurekweb@gmail.com>
2022-08-12 14:19:00 +0200

    add video trailer for film card

--- project/src/components/catalog/catalog-film-card/catalog-film-card.tsx
+++ project/src/components/catalog/catalog-film-card/catalog-film-card.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import type { FilmItem } from '../../../types/film-types';
 
@@ -7,11 +8,29 @@ type CatalogFilmCardProps = {
 }
 
 function CatalogFilmCard({filmItem}: CatalogFilmCardProps):JSX.Element {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const onActive = (): void => {
+    setIsActive(() => true);
+  };
+
+  const offActive = (): void => {
+    setIsActive(() => false);
+  };
 
   return(
-    <article className="small-film-card catalog__films-card">
+    <article className="small-film-card catalog__films-card"
+      onMouseEnter={onActive}
+      onMouseLeave={offActive}
+    >
       <div className="small-film-card__image">
-        <img src={filmItem.preview_image} alt={filmItem.name} width="280" height="175" />
+        {
+          isActive ?
+            <video src={filmItem.preview_video_link} className="player__video" autoPlay poster="img/player-poster.jpg"></video> :
+            <img src={filmItem.preview_image} alt={filmItem.name} width="280" height="175" />
+        }
+
+        {/* <img src={filmItem.preview_image} alt={filmItem.name} width="280" height="175" /> */}
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${filmItem.id}`}>{filmItem.name}</Link>

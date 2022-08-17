e88782c9fb11a32672459f3454995b12bf20fa6b
AntonOshurek <oshurekweb@gmail.com>
2022-08-17 13:15:10 +0200

    add my list link for user component

--- project/src/components/default-components/user-block/user-block.tsx
+++ project/src/components/default-components/user-block/user-block.tsx
@@ -1,11 +1,17 @@
-function UserBlock(): JSX.Element {
+import { Link } from 'react-router-dom';
 
+import { AppRoute } from '../../../consts/consts';
+
+function UserBlock(): JSX.Element {
   return (
     <ul className="user-block">
       <li className="user-block__item">
         <div className="user-block__avatar">
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          <Link  to={AppRoute.MY_LIST}>
+            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          </Link>
         </div>
+
       </li>
       <li className="user-block__item">
         <a className="user-block__link" href="link">Sign out</a>

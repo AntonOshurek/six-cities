2189c3b66cc6989489101561aacfb1f0680a3afb
AntonOshurek <oshurekweb@gmail.com>
2022-08-17 12:58:27 +0200

    move setAllFilms logick to redux from main page to app component

--- project/src/pages/main-page.tsx
+++ project/src/pages/main-page.tsx
@@ -1,30 +1,13 @@
 import MainPageBannerFilmCard from '../components/main-page-components/main-page-banner-film-card/main-page-banner-film-card';
 import MainPageContent from '../components/main-page-components/main-page-content/main-page-content';
 
-import type { FilmItem, FilmCardData } from '../types/film-types';
-//REDUX
-import { bindActionCreators, Dispatch } from 'redux';
-import { connect, ConnectedProps } from 'react-redux';
-import { setAllFilms } from '../store/actions/actions';
-import { Actions } from '../types/actions-types';
-
-const mapDispatchToProps = (dispatch: Dispatch<Actions>) => bindActionCreators({
-  fooAllFilms: setAllFilms,
-}, dispatch);
-
-const connector = connect(null, mapDispatchToProps);
+import type { FilmCardData } from '../types/film-types';
 
 type MainPageProps = {
   filmCard: FilmCardData,
-  allFilms: FilmItem[],
 }
 
-type PropsFromRedux = ConnectedProps<typeof connector>;
-type ConnectedComponentProps = PropsFromRedux & MainPageProps;
-
-function MainPage({ filmCard, allFilms, fooAllFilms }: ConnectedComponentProps): JSX.Element {
-  fooAllFilms(allFilms);
-
+function MainPage({ filmCard }: MainPageProps): JSX.Element {
   return (
     <>
       <MainPageBannerFilmCard filmCard={filmCard}/>
@@ -33,5 +16,4 @@ function MainPage({ filmCard, allFilms, fooAllFilms }: ConnectedComponentProps):
   );
 }
 
-export {MainPage};
-export default connector(MainPage);
+export default MainPage;
